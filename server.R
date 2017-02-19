705d4a47b95633f4ad245c74417a858c7dc1f561
hirogami <shibatau@gmail.com>
2017-02-20 07:49:09 +0900

    Create server.R

---
+++ server.R
@@ -0,0 +1,39 @@
+library(shiny)
+data0 <- read.csv("http://pastebin.com/raw/ikf16vAQ")
+library(dplyr)
+library(magrittr)
+
+# Define server logic for random distribution application
+shinyServer(function(input, output) {
+  
+  # Reactive expression to generate the requested distribution. This is 
+  # called whenever the inputs change. The renderers defined 
+  # below then all use the value computed from this expression
+  data <- reactive({  
+    dist <- switch(input$dist,
+                   year1 = filter(data0,year==1),
+                   year2 = filter(data0,year==2),
+                   year3 = filter(data0,year==3),
+                   yearall = data0,
+                   rnorm)
+    
+    dist[,"marks"]
+
+  })
+  
+  # Generate a plot of the data. Also uses the inputs to build the 
+  # plot label. Note that the dependencies on both the inputs and
+  # the 'data' reactive expression are both tracked, and all expressions 
+  # are called in the sequence implied by the dependency graph
+  output$plot <- renderPlot({
+    dist <- input$dist
+    n <- input$n
+    hist(data(), 
+         main=paste(dist))
+  }) 
+  
+  # Generate a summary of the data
+  output$summary <- renderPrint({
+    summary(data())
+      })
+})
